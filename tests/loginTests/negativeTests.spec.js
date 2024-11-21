a26ddececb052d7252fe2a6634cb11667015a2e6
Netanel Harush <netanelharush2@gmail.com>
2024-11-21 13:53:20 +0200

    example of parameterize test with destruction

--- tests/loginTests/negativeTests.spec.js
+++ tests/loginTests/negativeTests.spec.js
@@ -1,25 +1,20 @@
-import { expect, test } from "@playwright/test";
-const nUsers = [
-    { username: "standard_user", password: "59559" },
-    { username: "standard_users", password: "secret_sauce" },
-    { username: "standard_users", password: "fdhfhdufud" },
-    { username: "", password: "secret_sauce" },
-    { username: "error_user", password: "" },
-    { username: "", password: "" },
-  ];
-const siteURL = "https://www.saucedemo.com/";
+import {expect, test} from '@playwright/test'
+import {negativeUsers} from '../utils/testData.js'
 
-//Negative Test1
-test.describe.only("negative Login with", () => {
-  for (const user of nUsers) {
-    test(`enter to site and login with ${user.username} and ${user.password} `, async ({
-      page,
-    }) => {
-      await page.goto(siteURL);
-      await page.locator('[data-test="username"]').fill(nUsers.username);
-      await page.locator('[data-test="password"]').fill(nUsers.password);
-      await page.locator('[data-test="login-button"]').click();
-      await expect(page.locator('[data-test="error"]')).toBeVisible();
-    });
-  }
-});
+const siteURL = 'https://www.saucedemo.com/'
+
+test.describe.only('negative Login with', () => {
+    for (const {username, password, message} of negativeUsers) {
+        test(`enter to site and login with ${username} and ${password}`, async ({
+            page,
+        }) => {
+            await page.goto(siteURL)
+            await page.locator('[data-test="username"]').fill(username)
+            await page.locator('[data-test="password"]').fill(password)
+            await page.locator('[data-test="login-button"]').click()
+            await expect(page.locator('[data-test="error"]')).toHaveText(
+                message,
+            )
+        })
+    }
+})
--- tests/utils/testData.js
+++ tests/utils/testData.js
@@ -1,21 +1,26 @@
-import exp from "constants";
+import exp from 'constants'
 
-export const siteURL = "https://www.saucedemo.com/";
+export const siteURL = 'https://www.saucedemo.com/'
 
 export const users = [
-  { username: "standard_user", password: "secret_sauce" },
-  { username: "problem_user", password: "secret_sauce" },
-  { username: "performance_glitch_user", password: "secret_sauce" },
-  { username: "error_user", password: "secret_sauce" },
-  { username: "visual_user", password: "secret_sauce" },
-];
+    {username: 'standard_user', password: 'secret_sauce'},
+    {username: 'problem_user', password: 'secret_sauce'},
+    {username: 'performance_glitch_user', password: 'secret_sauce'},
+    {username: 'error_user', password: 'secret_sauce'},
+    {username: 'visual_user', password: 'secret_sauce'},
+]
 
-export const nUsers = [
-  { username: "standard_user", password: "59559" },
-  { username: "standard_users", password: "secret_sauce" },
-  { username: "standard_users", password: "fdhfhdufud" },
-  { username: "", password: "secret_sauce" },
-  { username: "error_user", password: "" },
-  { username: "", password: "" },
-  { username: "locked_out_user", password: "secret_sauce" },
-];
+export const negativeUsers = [
+    {
+        username: 'standard_user',
+        password: '59559',
+        message:
+            'Epic sadface: Username and password do not match any user in this service',
+    },
+    {username: 'standard_users', password: 'secret_sauce'},
+    {username: 'standard_users', password: 'fdhfhdufud'},
+    {username: '', password: 'secret_sauce'},
+    {username: 'error_user', password: ''},
+    {username: '', password: ''},
+    {username: 'locked_out_user', password: 'secret_sauce'},
+]
